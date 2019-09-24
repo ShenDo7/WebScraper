0c33844805d1006ba3a2430809dcf04e14466984
shendo7 <rodneyrjames@gmail.com>
2019-09-23 19:55:33 -0700

    test

--- public/scrape.js
+++ public/scrape.js
@@ -41,8 +41,8 @@ $(document).on("click", ".col-lg-10", function() {
         "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
       );
 
-       // If there's a note in the article
-       if (data.notes.length) {
+      // If there's a note in the article
+      if (data.notes.length) {
         console.log("HAS A NOTE!");
         $.ajax({
           method: "GET",
@@ -55,41 +55,43 @@ $(document).on("click", ".col-lg-10", function() {
         });
       }
 
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
+      // When you click the savenote button
+      $(document).on("click", "#savenote", function() {
+        // Grab the id associated with the article from the submit button
+        var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+        // Run a POST request to change the note, using what's entered in the inputs
+        $.ajax({
+          method: "POST",
+          url: "/articles/" + thisId,
+          data: {
+            // Value taken from title input
+            title: $("#titleinput").val(),
+            // Value taken from note textarea
+            body: $("#bodyinput").val()
+          }
+        })
+          // With that done
+          .then(function(data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#notes").empty();
+          });
+
+        // Also, remove the values entered in the input and textarea for note entry
+        $("#titleinput").val("");
+        $("#bodyinput").val("");
+      });
+
+      $(document).on("click", ".save", function() {
+        console.log($(this).attr("id"));
+        $.post("/article/save/" + $(this).attr("id"));
+      });
+
+      $(document).on("click", ".delete", function() {
+        console.log($(this).attr("id"));
+        $.post("/article/delete/" + $(this).attr("id"));
+      });
     });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
-
-$(document).on("click", ".save", function() {
-  console.log($(this).attr("id"));
-  $.post("/article/save/" + $(this).attr("id"));
-});
-
-$(document).on("click", ".delete", function() {
-  console.log($(this).attr("id"));
-  $.post("/article/delete/" + $(this).attr("id"));
 });
